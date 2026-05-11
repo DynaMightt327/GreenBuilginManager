b2609296b87a6c5b65baaaa023297c69cb23cb2b
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-05-11 09:09:40 -0500

    clases

--- src/co/edu/unbosque/model/Persona.java
+++ src/co/edu/unbosque/model/Persona.java
@@ -1,5 +1,73 @@
 package co.edu.unbosque.model;
 
 public class Persona {
+	
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private String genero;
+	private String correo;
+	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(String nombre, String apellido, int edad, String genero, String correo) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.genero = genero;
+		this.correo = correo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+	
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", genero=" + genero
+				+ ", correo=" + correo + "]";
+	}
+	
+	
 
 }
